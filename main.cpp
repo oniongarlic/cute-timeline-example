0490f9ef0487f975a63d238fe038ccea044a5d9d
Kaj-Michael Lang <milang@tal.org>
2024-03-30 19:30:07 +0200

    Set UI style

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickStyle>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationDomain("org.tal.timline-test");
+    QCoreApplication::setOrganizationName("tal.org");
+    QCoreApplication::setApplicationName("TimelineTest");
+    QCoreApplication::setApplicationVersion("0.1");
+
+    QQuickStyle::setStyle("Universal");
 
     QQmlApplicationEngine engine;
     QObject::connect(
@@ -12,6 +20,7 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+
     engine.loadFromModule("timelinetest", "Main");
 
     return app.exec();
